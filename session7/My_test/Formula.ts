0f59eb756e15ce4dded626ee313016aa0245e022
farhad tayebipour <tayebipourf@gmail.com>
2023-10-01 14:06:58 +0330

    Sesstion7_test

---
+++ session7/My_test/Formula.ts
@@ -0,0 +1,30 @@
+
+
+
+export function Radius(radius_number:number)
+{
+    
+    const radius=2*3.14*radius_number;
+    
+    const half_circle = (half_number:number) =>{ const half_circle=half_number+ radius;
+    console.log("RADIUS ="+" "+ radius + " / MOHIT NEFE Da= "+half_circle);}
+    
+    
+    return half_circle;
+}
+
+
+export function Square_perimeter(Square_number:number)
+{
+    
+    const Square= 4 * Square_number;
+    
+    const Square_area = () =>{ const area= Square_number*Square;
+    console.log("MOHIT ="+" "+ Square + " / MASAHAT= "+area);}
+    
+    
+    return Square_area;
+}
+
+
+
---
+++ session7/My_test/main.ts
@@ -0,0 +1,7 @@
+import {Radius,Square_perimeter} from "./Formula.ts";
+console.log("show my list");
+
+const show_raius= Radius;
+Radius(1)(2);
+const output_Square= Square_perimeter;
+output_Square(2)();
